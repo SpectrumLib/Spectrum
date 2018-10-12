11cd032b4b7219f0f1032bfb67c62d775777fef9
Sean Moss <moss.seank@gmail.com>
2018-10-12 11:19:09 -0600

    Now With Framework Detection™

--- Spectrum/Core/Platform.cs
+++ Spectrum/Core/Platform.cs
@@ -14,6 +14,16 @@ namespace Spectrum
 	}
 
 	/// <summary>
+	/// Represents the different .NET frameworks that the library can run on.
+	/// </summary>
+	public enum PlatformFramework
+	{
+		Core,
+		Framework,
+		Native
+	}
+
+	/// <summary>
 	/// Contains values and checks for working with different operating systems.
 	/// </summary>
 	public static class Platform
@@ -21,29 +31,38 @@ namespace Spectrum
 		/// <summary>
 		/// The current operating system.
 		/// </summary>
-		public static readonly PlatformOS Current;
+		public static readonly PlatformOS OS;
+		/// <summary>
+		/// The current framework running the code.
+		/// </summary>
+		public static readonly PlatformFramework Framework;
 		/// <summary>
 		/// Gets if the current operating system is Microsoft Windows.
 		/// </summary>
-		public static bool IsWindows => Current == PlatformOS.Windows;
+		public static bool IsWindows => OS == PlatformOS.Windows;
 		/// <summary>
 		/// Gets if the current operating system is a type of Linux.
 		/// </summary>
-		public static bool IsLinux => Current == PlatformOS.Linux;
+		public static bool IsLinux => OS == PlatformOS.Linux;
 		/// <summary>
 		/// Gets if the current operating system is desktop MacOSX.
 		/// </summary>
-		public static bool IsOSX => Current == PlatformOS.OSX;
+		public static bool IsOSX => OS == PlatformOS.OSX;
 		/// <summary>
 		/// Gets if the current operating system is a Unix derivative (not Windows).
 		/// </summary>
-		public static bool IsUnix => Current != PlatformOS.Windows;
+		public static bool IsUnix => OS != PlatformOS.Windows;
 
 		static Platform()
 		{
-			Current = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? PlatformOS.Windows :
-					  RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ? PlatformOS.OSX :
-					  PlatformOS.Linux;
+			OS = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? PlatformOS.Windows :
+				 RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ? PlatformOS.OSX :
+				 PlatformOS.Linux;
+
+			string sname = RuntimeInformation.FrameworkDescription;
+			Framework = sname.Contains("Core") ? PlatformFramework.Core :
+						sname.Contains("Framework") ? PlatformFramework.Framework : 
+						PlatformFramework.Native;
 		}
 	}
 }
