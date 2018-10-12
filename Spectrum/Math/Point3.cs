0c586f9d149675d0d16e9c4979f1a86a416c0d79
Sean Moss <moss.seank@gmail.com>
2018-10-12 14:34:36 -0600

    Math (2/?): Point3 type, min/max/casting for both point types

--- Spectrum/Math/Point3.cs
+++ Spectrum/Math/Point3.cs
@@ -4,14 +4,14 @@ using System.Runtime.CompilerServices;
 namespace Spectrum
 {
 	/// <summary>
-	/// Defines a location on a 2D cartesian integer grid.
+	/// Defines a location on a 3D cartesian integer grid.
 	/// </summary>
-	public struct Point : IEquatable<Point>
+	public struct Point3 : IEquatable<Point3>
 	{
 		/// <summary>
-		/// Represents the origin of the grid, with both components as zero.
+		/// Represents the origin of the grid, with all components as zero.
 		/// </summary>
-		public static readonly Point Zero = new Point(0, 0);
+		public static readonly Point3 Zero = new Point3(0, 0, 0);
 
 		#region Fields
 		/// <summary>
@@ -22,15 +22,19 @@ namespace Spectrum
 		/// The y-coordinate of the point.
 		/// </summary>
 		public int Y;
+		/// <summary>
+		/// The z-coordinate of the point.
+		/// </summary>
+		public int Z;
 		#endregion // Fields
 
 		/// <summary>
-		/// Creates a point with both components equal to the passed value.
+		/// Creates a point with all components equal to the passed value.
 		/// </summary>
-		/// <param name="i">The value of both components.</param>
-		public Point(int i)
+		/// <param name="i">The value of all components.</param>
+		public Point3(int i)
 		{
-			X = Y = i;
+			X = Y = Z = i;
 		}
 
 		/// <summary>
@@ -38,15 +42,29 @@ namespace Spectrum
 		/// </summary>
 		/// <param name="x">The x-component.</param>
 		/// <param name="y">The y-component.</param>
-		public Point(int x, int y)
+		/// <param name="z">The z-component.</param>
+		public Point3(int x, int y, int z)
 		{
 			X = x;
 			Y = y;
+			Z = z;
+		}
+
+		/// <summary>
+		/// Creates a point from a 2D point appended with a z-axis coordinate.
+		/// </summary>
+		/// <param name="p">The 2D point for the X and Y coordinates.</param>
+		/// <param name="z">The z coordinate.</param>
+		public Point3(in Point p, int z)
+		{
+			X = p.X;
+			Y = p.Y;
+			Z = z;
 		}
 
 		public override bool Equals(object obj)
 		{
-			return (obj as Point?)?.Equals(this) ?? false;
+			return (obj as Point3?)?.Equals(this) ?? false;
 		}
 
 		public override int GetHashCode()
@@ -56,13 +74,14 @@ namespace Spectrum
 				int hash = 17;
 				hash = (hash * 23) + X;
 				hash = (hash * 23) + Y;
+				hash = (hash * 23) + Z;
 				return hash;
 			}
 		}
 
 		public override string ToString()
 		{
-			return $"{{{X} {Y}}}";
+			return $"{{{X} {Y} {Z}}}";
 		}
 
 		#region Distance
@@ -70,37 +89,37 @@ namespace Spectrum
 		/// Returns the distance from the point to the origin.
 		/// </summary>
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public float Distance() => (float)Math.Sqrt(X * X + Y * Y);
+		public float Distance() => (float)Math.Sqrt(X * X + Y * Y + Z * Z);
 
 		/// <summary>
 		/// Returns the distance from the point to the origin.
 		/// </summary>
 		/// <param name="p">The point.</param>
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static float Distance(in Point p) => (float)Math.Sqrt(p.X * p.X + p.Y * p.Y);
+		public static float Distance(in Point3 p) => (float)Math.Sqrt(p.X * p.X + p.Y * p.Y + p.Z * p.Z);
 
 		/// <summary>
 		/// Returns the distance squared from the point to the origin.
 		/// </summary>
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public int DistanceSquared() => X * X + Y * Y;
+		public int DistanceSquared() => X * X + Y * Y + Z * Z;
 
 		/// <summary>
 		/// Returns the distance squared from the point to the origin.
 		/// </summary>
 		/// <param name="p">The point.</param>
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static int DistanceSquared(in Point p) => p.X * p.X + p.Y * p.Y;
+		public static int DistanceSquared(in Point3 p) => p.X * p.X + p.Y * p.Y + p.Z * p.Z;
 
 		/// <summary>
 		/// Gets the distance between this point and another.
 		/// </summary>
 		/// <param name="p">The point to get the distance to.</param>
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public float DistanceTo(in Point p)
+		public float DistanceTo(in Point3 p)
 		{
-			int dx = X - p.X, dy = Y - p.Y;
-			return (float)Math.Sqrt(dx * dx + dy * dy);
+			int dx = X - p.X, dy = Y - p.Y, dz = Z - p.Z;
+			return (float)Math.Sqrt(dx * dx + dy * dy + dz * dz);
 		}
 
 		/// <summary>
@@ -109,10 +128,10 @@ namespace Spectrum
 		/// <param name="l">The first point.</param>
 		/// <param name="r">The second point.</param>
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static float DistanceTo(in Point l, in Point r)
+		public static float DistanceTo(in Point3 l, in Point3 r)
 		{
-			int dx = l.X - r.X, dy = l.Y - r.Y;
-			return (float)Math.Sqrt(dx * dx + dy * dy);
+			int dx = l.X - r.X, dy = l.Y - r.Y, dz = l.Z - r.Z;
+			return (float)Math.Sqrt(dx * dx + dy * dy + dz * dz);
 		}
 
 		/// <summary>
@@ -120,10 +139,10 @@ namespace Spectrum
 		/// </summary>
 		/// <param name="p">The point to get the distance to.</param>
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public float DistanceToSquared(in Point p)
+		public float DistanceToSquared(in Point3 p)
 		{
-			int dx = X - p.X, dy = Y - p.Y;
-			return dx * dx + dy * dy;
+			int dx = X - p.X, dy = Y - p.Y, dz = Z - p.Z;
+			return dx * dx + dy * dy + dz * dz;
 		}
 
 		/// <summary>
@@ -132,73 +151,73 @@ namespace Spectrum
 		/// <param name="l">The first point.</param>
 		/// <param name="r">The second point.</param>
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static float DistanceToSquared(in Point l, in Point r)
+		public static float DistanceToSquared(in Point3 l, in Point3 r)
 		{
-			int dx = l.X - r.X, dy = l.Y - r.Y;
-			return dx * dx + dy * dy;
+			int dx = l.X - r.X, dy = l.Y - r.Y, dz = l.Z - r.Z;
+			return dx * dx + dy * dy + dz * dz;
 		}
 		#endregion // Distance
 
 		#region IEquatable
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		bool IEquatable<Point>.Equals(Point other)
+		bool IEquatable<Point3>.Equals(Point3 other)
 		{
-			return (X == other.X) && (Y == other.Y);
+			return (X == other.X) && (Y == other.Y) && (Z == other.Z);
 		}
 		#endregion // IEquatable
 
 		#region Operators
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static bool operator == (in Point l, in Point r)
+		public static bool operator == (in Point3 l, in Point3 r)
 		{
-			return (l.X == r.X) && (l.Y == r.Y);
+			return (l.X == r.X) && (l.Y == r.Y) && (l.Z == r.Z);
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static bool operator != (in Point l, in Point r)
+		public static bool operator != (in Point3 l, in Point3 r)
 		{
-			return (l.X != r.X) || (l.Y != r.Y);
+			return (l.X != r.X) || (l.Y != r.Y) || (l.Z != r.Z);
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static Point operator + (in Point l, in Point r)
+		public static Point3 operator + (in Point3 l, in Point3 r)
 		{
-			return new Point(l.X + r.X, l.Y + r.Y);
+			return new Point3(l.X + r.X, l.Y + r.Y, l.Z + r.Z);
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static Point operator - (in Point l, in Point r)
+		public static Point3 operator - (in Point3 l, in Point3 r)
 		{
-			return new Point(l.X - r.X, l.Y - r.Y);
+			return new Point3(l.X - r.X, l.Y - r.Y, l.Z - r.Z);
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static Point operator * (in Point l, int r)
+		public static Point3 operator * (in Point3 l, int r)
 		{
-			return new Point(l.X * r, l.Y * r);
+			return new Point3(l.X * r, l.Y * r, l.Z * r);
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static Point operator * (int l, in Point r)
+		public static Point3 operator * (int l, in Point3 r)
 		{
-			return new Point(l * r.X, l * r.Y);
+			return new Point3(l * r.X, l * r.Y, l * r.Z);
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static Point operator / (in Point l, int r)
+		public static Point3 operator / (in Point3 l, int r)
 		{
-			return new Point(l.X / r, l.Y / r);
+			return new Point3(l.X / r, l.Y / r, l.Z / r);
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static implicit operator Point3 (in Point p)
+		public static explicit operator Point (in Point3 p)
 		{
-			return new Point3(p.X, p.Y, 0);
+			return new Point(p.X, p.Y);
 		}
 
 		// TODO
 		//[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		//public static implicit operator Vec2 (in Point p)
+		//public static implicit operator Vec3 (in Point3 p)
 		//{
 		//	return new Vec2(p.X, p.Y);
 		//}
@@ -211,9 +230,9 @@ namespace Spectrum
 		/// <param name="l">The first point.</param>
 		/// <param name="r">The second point.</param>
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static Point Min(in Point l, in Point r)
+		public static Point3 Min(in Point3 l, in Point3 r)
 		{
-			return new Point(l.X < r.X ? l.X : r.X, l.Y < r.Y ? l.Y : r.Y);
+			return new Point3(l.X < r.X ? l.X : r.X, l.Y < r.Y ? l.Y : r.Y, l.Z < r.Z ? l.Z : r.Z);
 		}
 		/// <summary>
 		/// Creates a new point with the minimum components of the two input points.
@@ -222,9 +241,9 @@ namespace Spectrum
 		/// <param name="r">The second point.</param>
 		/// <param name="p">The output value for the minimized point.</param>
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static void Min(in Point l, in Point r, out Point p)
+		public static void Min(in Point3 l, in Point3 r, out Point3 p)
 		{
-			p = new Point(l.X < r.X ? l.X : r.X, l.Y < r.Y ? l.Y : r.Y);
+			p = new Point3(l.X < r.X ? l.X : r.X, l.Y < r.Y ? l.Y : r.Y, l.Z < r.Z ? l.Z : r.Z);
 		}
 
 		/// <summary>
@@ -233,9 +252,9 @@ namespace Spectrum
 		/// <param name="l">The first point.</param>
 		/// <param name="r">The second point.</param>
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static Point Max(in Point l, in Point r)
+		public static Point3 Max(in Point3 l, in Point3 r)
 		{
-			return new Point(l.X > r.X ? l.X : r.X, l.Y > r.Y ? l.Y : r.Y);
+			return new Point3(l.X > r.X ? l.X : r.X, l.Y > r.Y ? l.Y : r.Y, l.Z > r.Z ? l.Z : r.Z);
 		}
 		/// <summary>
 		/// Creates a new point with the maximum components of the two input points.
@@ -244,9 +263,9 @@ namespace Spectrum
 		/// <param name="r">The second point.</param>
 		/// <param name="p">The output value for the maximized point.</param>
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static void Max(in Point l, in Point r, out Point p)
+		public static void Max(in Point3 l, in Point3 r, out Point3 p)
 		{
-			p = new Point(l.X > r.X ? l.X : r.X, l.Y > r.Y ? l.Y : r.Y);
+			p = new Point3(l.X > r.X ? l.X : r.X, l.Y > r.Y ? l.Y : r.Y, l.Z > r.Z ? l.Z : r.Z);
 		}
 		#endregion // Min/Max
 	}
