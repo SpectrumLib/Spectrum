e5d2b7e1399600313a9a0da956bdfe6ccede6961
Sean Moss <moss.seank@gmail.com>
2019-01-27 12:40:08 -0700

    Type for describing render pass attachments, and initial R.P. type

---
+++ Spectrum/Graphics/RenderPass/AttachmentInfo.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Vk = VulkanCore;
+
+namespace Spectrum.Graphics
+{
+	/// <summary>
+	/// Describes how a texture resource is used as a <see cref="RenderPass"/> attachment.
+	/// </summary>
+	public struct AttachmentInfo
+	{
+		#region Fields
+		/// <summary>
+		/// The name of the attachment, used to uniquely identify it within a renderpass.
+		/// </summary>
+		public readonly string Name;
+		/// <summary>
+		/// The format of the attachment's texels.
+		/// </summary>
+		public readonly TexelFormat Format;
+		/// <summary>
+		/// The operation to perform on an attachment when it is prepared for a render pass.
+		/// </summary>
+		public readonly AttachmentOp LoadOp;
+		/// <summary>
+		/// If the contents of the attachment need to be preserved after the render pass completes.
+		/// </summary>
+		public readonly bool Preserve;
+		#endregion // Fields
+
+		/// <summary>
+		/// Creates a new description for a render pass attachment.
+		/// </summary>
+		/// <param name="name">The name of the attachment. Must be non-null, not empty, and unique within a render pass instance.</param>
+		/// <param name="format">The texel format for the attachment.</param>
+		/// <param name="op">The load operation for the attachment.</param>
+		/// <param name="preserve">If the attachment's contents should be preserved past the end of the render pass.</param>
+		public AttachmentInfo(string name, TexelFormat format, AttachmentOp op, bool preserve)
+		{
+			if (String.IsNullOrWhiteSpace(name))
+				throw new ArgumentException("The name for an attachment cannot be null or whitespace");
+
+			Name = name;
+			Format = format;
+			LoadOp = op;
+			Preserve = preserve;
+		}
+	}
+
+	/// <summary>
+	/// Operations that can be performed on render pass attachments when they are loaded for use.
+	/// </summary>
+	public enum AttachmentOp
+	{
+		/// <summary>
+		/// The existing contents of the attachment are preserved.
+		/// </summary>
+		Preserve = Vk.AttachmentLoadOp.Load,
+		/// <summary>
+		/// All texels in the attachment are cleared to a constant value.
+		/// </summary>
+		Clear = Vk.AttachmentLoadOp.Clear,
+		/// <summary>
+		/// The existing contents of the attachment are not important, Vulkan is allowed to do whatever it wants with
+		/// them before we write to or read from the attachment.
+		/// </summary>
+		Discard = Vk.AttachmentLoadOp.DontCare
+	}
+}
---
+++ Spectrum/Graphics/RenderPass/RenderPass.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Spectrum.Graphics
+{
+	/// <summary>
+	/// Fully defines the attachments, shaders, states, and output of a rendering pipeline, with the ability to split
+	/// rendering up into multiple subpasses. Rendering cannot happen without a valid RenderPass instance bound.
+	/// <para>
+	/// RenderPass construction needs to happen in a very specific order, using the <c>Add...()</c> functions to
+	/// build up a full description of the RenderPass, and then calling <see cref="Build"/>. Creating a render pass
+	/// is very verbose and time consuming, but the tradeoff is a fully defined rendering state that can be strongly
+	/// optimized by the driver when it is created, and quickly and efficiently bound at runtime with minimal
+	/// backend work.
+	/// </para>
+	/// </summary>
+	public sealed class RenderPass : IDisposable
+	{
+		#region Fields
+		/// <summary>
+		/// If the instance has been built using <see cref="Build"/> and is ready for use in rendering.
+		/// </summary>
+		public bool Prepared { get; private set; } = false;
+
+		private bool _isDisposed = false;
+		#endregion // Fields
+
+		public RenderPass()
+		{
+
+		}
+		~RenderPass()
+		{
+			dispose(false);
+		}
+
+		/// <summary>
+		/// Fully constructs the renderpass, all of its subpasses, allocates texture resources as necessary, and
+		/// binds pipelines, shaders, and attachments.
+		/// </summary>
+		public void Build()
+		{
+			if (Prepared)
+				return;
+
+			Prepared = true;
+		}
+
+		#region IDisposable
+		public void Dispose()
+		{
+			dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		private void dispose(bool disposing)
+		{
+			if (!_isDisposed)
+			{
+
+			}
+			_isDisposed = true;
+		}
+		#endregion // IDisposable
+	}
+}
