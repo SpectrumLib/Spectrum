fd470a4309b4d03805cc34c5599cabf7dc4b0b24
Sean Moss <moss.seank@gmail.com>
2019-02-02 09:20:14 -0700

    Initial .NET Framework 4.6.1 project for the Prism app and interface

---
+++ Prism/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prism
+{
+	public static class Program
+	{
+		public static void Main(string[] args)
+		{
+
+		}
+	}
+}
---
+++ Prism/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Prism")]
+[assembly: AssemblyDescription("Application for operating on content projects and files for the Spectrum library.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("N/A")]
+[assembly: AssemblyProduct("Spectrum")]
+[assembly: AssemblyCopyright("Copyright © Sean Moss 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("0.1.0")]
