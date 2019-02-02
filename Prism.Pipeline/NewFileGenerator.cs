7e4001e176213a9053f9bc71ee292b0e9fec8ecc
Sean Moss <moss.seank@gmail.com>
2019-02-02 16:21:52 -0700

    Default file for an empty Prism project

---
+++ Prism.Pipeline/NewFileGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace Prism
+{
+	// Creates new default project and content files
+	internal static class NewFileGenerator
+	{
+		private static readonly string NEW_PROJECT_RES = "Prism.Resources.Default.prism";
+
+		// Creates a new default, empty project file at the given path
+		public static void NewProjectFile(string path)
+		{
+			// If a directory is specified, append the file name
+			string ext = Path.GetExtension(path);
+			if (ext.Length == 0)
+				path = Path.Combine(path, "Content.prism");
+			else if (ext != ".prism")
+				Console.WriteLine("WARNING: the file path '{path}' does not end in the standard '.prism' extension.");
+
+			// Check if it already exists
+			path = Path.GetFullPath(path);
+			var pathDir = Path.GetDirectoryName(path);
+			if (File.Exists(path))
+				throw new Exception("A file already exists at the given path");
+			if (!Directory.Exists(pathDir))
+				Directory.CreateDirectory(pathDir);
+
+			// Write the embedded resource as the new file
+			using (var reader = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream(NEW_PROJECT_RES)))
+			using (var writer = new StreamWriter(File.Open(path, FileMode.CreateNew, FileAccess.Write, FileShare.None), Encoding.Unicode))
+			{
+				var raw = reader.ReadToEnd();
+				writer.Write(raw);
+			}
+		}
+	}
+}
