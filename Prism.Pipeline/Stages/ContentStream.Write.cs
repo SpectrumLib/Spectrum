542a755c2542b2970955df438d91c95a7e1169ec
Sean Moss <moss.seank@gmail.com>
2019-02-06 21:02:15 -0700

    Implemented writing functions in ContentStream with efficient buffering

---
+++ Prism.Pipeline/Stages/ContentStream.Write.cs
@@ -0,0 +1,265 @@
+﻿using System;
+
+namespace Prism
+{
+	// To remain compatible with the BinaryReader class, all Write() functionality should be taken from the CoreCLR
+	//   BinaryWriter class at:
+	//   https://github.com/dotnet/coreclr/blob/master/src/System.Private.CoreLib/shared/System/IO/BinaryWriter.cs
+	public sealed partial class ContentStream
+    {
+		public void Write(bool val)
+		{
+			if ((_bufferPos + 1) > BUFFER_SIZE)
+				flushInternal();
+			_memBuffer[_bufferPos++] = (byte)(val ? 1 : 0);
+		}
+
+		public void Write(sbyte val)
+		{
+			if ((_bufferPos + 1) > BUFFER_SIZE)
+				flushInternal();
+			_memBuffer[_bufferPos++] = (byte)val;
+		}
+
+		public void Write(byte val)
+		{
+			if ((_bufferPos + 1) > BUFFER_SIZE)
+				flushInternal();
+			_memBuffer[_bufferPos++] = val;
+		}
+
+		public void Write(short val)
+		{
+			if ((_bufferPos + 2) > BUFFER_SIZE)
+				flushInternal();
+			_memBuffer[_bufferPos++] = (byte)val;
+			_memBuffer[_bufferPos++] = (byte)(val >> 8);
+		}
+
+		public void Write(ushort val)
+		{
+			if ((_bufferPos + 2) > BUFFER_SIZE)
+				flushInternal();
+			_memBuffer[_bufferPos++] = (byte)val;
+			_memBuffer[_bufferPos++] = (byte)(val >> 8);
+		}
+
+		public void Write(int val)
+		{
+			if ((_bufferPos + 4) > BUFFER_SIZE)
+				flushInternal();
+			_memBuffer[_bufferPos++] = (byte)val;
+			_memBuffer[_bufferPos++] = (byte)(val >> 8);
+			_memBuffer[_bufferPos++] = (byte)(val >> 16);
+			_memBuffer[_bufferPos++] = (byte)(val >> 24);
+		}
+
+		public void Write(uint val)
+		{
+			if ((_bufferPos + 4) > BUFFER_SIZE)
+				flushInternal();
+			_memBuffer[_bufferPos++] = (byte)val;
+			_memBuffer[_bufferPos++] = (byte)(val >> 8);
+			_memBuffer[_bufferPos++] = (byte)(val >> 16);
+			_memBuffer[_bufferPos++] = (byte)(val >> 24);
+		}
+
+		public void Write(long val)
+		{
+			if ((_bufferPos + 8) > BUFFER_SIZE)
+				flushInternal();
+			_memBuffer[_bufferPos++] = (byte)val;
+			_memBuffer[_bufferPos++] = (byte)(val >> 8);
+			_memBuffer[_bufferPos++] = (byte)(val >> 16);
+			_memBuffer[_bufferPos++] = (byte)(val >> 24);
+			_memBuffer[_bufferPos++] = (byte)(val >> 32);
+			_memBuffer[_bufferPos++] = (byte)(val >> 40);
+			_memBuffer[_bufferPos++] = (byte)(val >> 48);
+			_memBuffer[_bufferPos++] = (byte)(val >> 56);
+		}
+
+		public void Write(ulong val)
+		{
+			if ((_bufferPos + 8) > BUFFER_SIZE)
+				flushInternal();
+			_memBuffer[_bufferPos++] = (byte)val;
+			_memBuffer[_bufferPos++] = (byte)(val >> 8);
+			_memBuffer[_bufferPos++] = (byte)(val >> 16);
+			_memBuffer[_bufferPos++] = (byte)(val >> 24);
+			_memBuffer[_bufferPos++] = (byte)(val >> 32);
+			_memBuffer[_bufferPos++] = (byte)(val >> 40);
+			_memBuffer[_bufferPos++] = (byte)(val >> 48);
+			_memBuffer[_bufferPos++] = (byte)(val >> 56);
+		}
+
+		public unsafe void Write(float val)
+		{
+			if ((_bufferPos + 4) > BUFFER_SIZE)
+				flushInternal();
+			uint tmpval = *(uint*)&val;
+			_memBuffer[_bufferPos++] = (byte)tmpval;
+			_memBuffer[_bufferPos++] = (byte)(tmpval >> 8);
+			_memBuffer[_bufferPos++] = (byte)(tmpval >> 16);
+			_memBuffer[_bufferPos++] = (byte)(tmpval >> 24);
+		}
+
+		public unsafe void Write(double val)
+		{
+			if ((_bufferPos + 8) > BUFFER_SIZE)
+				flushInternal();
+			ulong tmpval = *(ulong*)&val;
+			_memBuffer[_bufferPos++] = (byte)tmpval;
+			_memBuffer[_bufferPos++] = (byte)(tmpval >> 8);
+			_memBuffer[_bufferPos++] = (byte)(tmpval >> 16);
+			_memBuffer[_bufferPos++] = (byte)(tmpval >> 24);
+			_memBuffer[_bufferPos++] = (byte)(tmpval >> 32);
+			_memBuffer[_bufferPos++] = (byte)(tmpval >> 40);
+			_memBuffer[_bufferPos++] = (byte)(tmpval >> 48);
+			_memBuffer[_bufferPos++] = (byte)(tmpval >> 56);
+		}
+
+		public unsafe void Write(decimal val)
+		{
+			if ((_bufferPos + 16) > BUFFER_SIZE)
+				flushInternal();
+
+			int* bits = (int*)&val;
+			_memBuffer[_bufferPos++] = (byte)bits[0];
+			_memBuffer[_bufferPos++] = (byte)(bits[0] >> 8);
+			_memBuffer[_bufferPos++] = (byte)(bits[0] >> 16);
+			_memBuffer[_bufferPos++] = (byte)(bits[0] >> 24);
+			_memBuffer[_bufferPos++] = (byte)bits[1];
+			_memBuffer[_bufferPos++] = (byte)(bits[1] >> 8);
+			_memBuffer[_bufferPos++] = (byte)(bits[1] >> 16);
+			_memBuffer[_bufferPos++] = (byte)(bits[1] >> 24);
+			_memBuffer[_bufferPos++] = (byte)bits[2];
+			_memBuffer[_bufferPos++] = (byte)(bits[2] >> 8);
+			_memBuffer[_bufferPos++] = (byte)(bits[2] >> 16);
+			_memBuffer[_bufferPos++] = (byte)(bits[2] >> 24);
+			_memBuffer[_bufferPos++] = (byte)bits[3];
+			_memBuffer[_bufferPos++] = (byte)(bits[3] >> 8);
+			_memBuffer[_bufferPos++] = (byte)(bits[3] >> 16);
+			_memBuffer[_bufferPos++] = (byte)(bits[3] >> 24);
+		}
+
+		public unsafe void Write(byte[] data, uint start = 0, uint count = UInt32.MaxValue)
+		{
+			if (data == null)
+				throw new ArgumentNullException(nameof(data));
+			if (start >= data.Length)
+				throw new ArgumentOutOfRangeException(nameof(start), "The starting index for the array was bigger than the array");
+			if (count == UInt32.MaxValue)
+				count = (uint)data.Length - start;
+			if ((start + count) > data.Length)
+				throw new ArgumentOutOfRangeException(nameof(count), "The array is not large enough to supply the requested amount of data");
+
+			bool direct = count >= DIRECT_WRITE_THRESHOLD;
+			if (direct || (_bufferPos + count) > BUFFER_SIZE)
+				flushInternal();
+
+			// Write direct to file if over threshold
+			if (direct)
+			{
+				fixed (byte* ptr = data)
+				{
+					flushDirect(ptr, count);
+				}
+			}
+			else
+			{
+				fixed (byte *src = data, dst = _memBuffer)
+				{
+					Buffer.MemoryCopy(src, dst, count, count);
+					_bufferPos += count;
+				}
+			}
+		}
+
+		public unsafe void Write(char val)
+		{
+			if (Char.IsSurrogate(val))
+				throw new ArgumentException("A unicode surrogate is not allowed as a single character", nameof(val));
+
+			var bytes = stackalloc byte[4];
+			int numBytes = _encoder.GetBytes(&val, 1, bytes, 4, true);
+
+			if ((_bufferPos + numBytes) > BUFFER_SIZE)
+				flushInternal();
+
+			fixed (byte *dst = _memBuffer)
+			{
+				Buffer.MemoryCopy(bytes, dst, 4, numBytes);
+				_bufferPos += (uint)numBytes;
+			}
+		}
+
+		public unsafe void Write(char[] chars, uint start = 0, uint count = UInt32.MaxValue)
+		{
+			if (chars == null)
+				throw new ArgumentNullException(nameof(chars));
+			if (start >= chars.Length)
+				throw new ArgumentOutOfRangeException(nameof(start), "The starting index for the array was bigger than the array");
+			if (count == UInt32.MaxValue)
+				count = (uint)chars.Length - start;
+			if ((start + count) > chars.Length)
+				throw new ArgumentOutOfRangeException(nameof(count), "The array is not large enough to supply the requested amount of data");
+
+			var bytes = stackalloc byte[chars.Length * 4]; // Assumed worst case scenario, but is time-efficient and not the end of the world
+			int numBytes;
+			fixed (char *src = chars)
+			{
+				numBytes = _encoder.GetBytes(src, (int)count, bytes, chars.Length * 4, true);
+			}
+
+			bool direct = numBytes >= DIRECT_WRITE_THRESHOLD;
+			if (direct || (_bufferPos + numBytes) > BUFFER_SIZE)
+				flushInternal();
+
+			// Write direct to file if over threshold
+			if (direct)
+				flushDirect(bytes, (uint)numBytes);
+			else
+			{
+				fixed (byte* dst = _memBuffer)
+				{
+					Buffer.MemoryCopy(bytes, dst, numBytes, numBytes);
+					_bufferPos += (uint)numBytes;
+				}
+			}
+		}
+
+		public unsafe void Write(string val)
+		{
+			if (val == null)
+				throw new ArgumentNullException(nameof(val));
+			uint numBytes = (uint)_encoding.GetByteCount(val);
+			// Needs to fit entirely into the available buffer (gross, probably fix this later)
+			//  Still, this equates to ~8mil characters in the worst case (4-byte code points), and you should rethink what you are doing
+			//  if you have a string that is this long (there are only a few published books in history longer than this)
+			if (numBytes > (BUFFER_SIZE - 4)) 
+				throw new ArgumentException("Cannot write a string whose bytes cannot fit in 32MB", nameof(val));
+
+			if ((_bufferPos + numBytes + 4) > BUFFER_SIZE) // +4 for the length encoding
+				flushInternal();
+
+			// Write the encoded length
+			{
+				uint tmpNum = numBytes;
+				while (tmpNum >= 0x80)
+				{
+					_memBuffer[_bufferPos++] = (byte)(tmpNum | 0x80);
+					tmpNum >>= 7;
+				}
+				_memBuffer[_bufferPos++] = (byte)tmpNum;
+			}
+
+			// Write the bytes directly into the buffer
+			fixed (char* src = val)
+			fixed (byte* dst = _memBuffer)
+			{
+				_encoding.GetBytes(src, val.Length, dst + _bufferPos, (int)numBytes);
+				_bufferPos += numBytes;
+			}
+		}
+	}
+}
--- Prism.Pipeline/Stages/ContentStream.cs
+++ Prism.Pipeline/Stages/ContentStream.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Prism
 {
@@ -7,27 +9,102 @@ namespace Prism
 	/// Stream type used be <see cref="ContentWriter{Tin}"/> instances to write the processed content data out to 
 	/// a content file in binary format.
 	/// </summary>
-	public sealed class ContentStream
+	public sealed partial class ContentStream
 	{
 		// Internally, a single instance of this type is provided to each BuildTask for a build engine. After a content
 		//   file is imported and processed, the underlying stream in this type is reset so the same buffer can be used
 		//   to write the new data. After the write is complete, the buffer is flushed to the file asynchronously so
 		//   the task can move onto the import and processing stages for the next file. The task will wait on the
-		//   async flush from the previous file before starting the write process for the next file.
+		//   async flush from the previous file before starting the write process for the next file. If the internal
+		//   memory buffer gets full before writing ends, then the stream will synchronously write the buffer out to
+		//   the file, and then continue writing from the beginning of the memory buffer.
 
 		private const uint BUFFER_SIZE = 33_554_432; // 32MB for the backing buffer size
+		private const uint DIRECT_WRITE_THRESHOLD = (uint)(BUFFER_SIZE * 0.9); // Arbitrary 90% threshold for direct write to disk
 
 		#region Fields
-		private byte[] _memBuffer = null;
-		private MemoryStream _baseStream = null;
-		private BinaryWriter _writer = null;
+		private readonly byte[] _memBuffer = null;
+		private uint _bufferPos = 0;
+
+		// The size (in bytes) of the file written by this stream, not valid until the final Flush() is called
+		internal uint OutputSize { get; private set; } = 0;
+
+		// The UTF8 encoding and associated encoder used to get string and character bytes
+		private readonly Encoding _encoding;
+		private readonly Encoder _encoder;
+
+		// The current async write task, if any
+		private Task _writeTask = null;
+		// The absolute path to the current output file
+		private string _currentFile = null;
 		#endregion // Fields
 
 		internal ContentStream()
 		{
 			_memBuffer = new byte[BUFFER_SIZE];
-			_baseStream = new MemoryStream(_memBuffer, 0, (int)BUFFER_SIZE, true);
-			_writer = new BinaryWriter(_baseStream);
+			_encoding = new UTF8Encoding(
+				encoderShouldEmitUTF8Identifier: false,
+				throwOnInvalidBytes: true
+			);
+			_encoder = _encoding.GetEncoder();
+		}
+
+		// Waits on the old write task, then resets the type to start recoding at the beginning of the buffer
+		internal void Reset(string path)
+		{
+			_writeTask?.Wait();
+			_bufferPos = 0;
+			OutputSize = 0;
+			_currentFile = path;
+		}
+
+		// Called by the task when it is done with this writer, launches the async write
+		internal void Flush()
+		{
+			OutputSize += _bufferPos;
+
+			_writeTask = new Task(() => {
+				using (var file = File.Open(_currentFile, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None))
+				{
+					file.Write(_memBuffer, 0, (int)_bufferPos);
+					file.Flush();
+				}
+			});
+			_writeTask.Start();
+		}
+
+		// Called by write functions when the backing buffer is full to synchronously write the buffer out and reset it
+		private void flushInternal()
+		{
+			OutputSize += _bufferPos;
+
+			// Synchronously write
+			using (var file = File.Open(_currentFile, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None))
+			{
+				file.Write(_memBuffer, 0, (int)_bufferPos);
+				file.Flush();
+			}
+
+			// Reset
+			_bufferPos = 0;
+		}
+
+		// Used to directly flush very large arrays directly to the file
+		//  !!! flushInternal() should be called before this or the data will get out of order !!!
+		private unsafe void flushDirect(byte* data, uint length)
+		{
+			OutputSize += length;
+
+			// Synchronously write
+			using (var buffer = new UnmanagedMemoryStream(data, length))
+			using (var file = File.Open(_currentFile, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None))
+			{
+				buffer.CopyTo(file);
+				file.Flush();
+			}
+
+			// Reset
+			_bufferPos = 0;
 		}
 	}
 }
