b65786dec57dfc254cb9a251a03051ce5dbfffa3
Sean Moss <moss.seank@gmail.com>
2019-02-06 21:38:32 -0700

    Passthrough pipeline stages, basically file copy/paste in code

---
+++ Prism.Pipeline/Builtin/PassthroughImporter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Prism.Builtin
+{
+	// Loads the file's contents into memory unaltered
+	[ContentImporter("Passthrough Importer", null, ".txt")]
+	internal sealed class PassthroughImporter : ContentImporter<byte[]>
+	{
+		public override byte[] Import(FileStream stream, ImporterContext ctx)
+		{
+			using (BinaryReader reader = new BinaryReader(stream))
+			{
+				byte[] data = new byte[ctx.FileLength];
+				reader.Read(data, 0, (int)ctx.FileLength);
+				return data;
+			}
+		}
+	}
+}
---
+++ Prism.Pipeline/Builtin/PassthroughWriter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Prism.Builtin
+{
+	// Writes the raw data passed to it directly to the output file unaltered.
+	internal sealed class PassthroughWriter : ContentWriter<byte[]>
+	{
+		public override void Write(byte[] input, ContentStream writer)
+		{
+			writer.Write(input);
+		}
+	}
+}
