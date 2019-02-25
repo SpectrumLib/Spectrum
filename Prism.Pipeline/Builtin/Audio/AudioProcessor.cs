8d884d33d7677fbdf18175f527417b2fb6784cd4
Sean Moss <moss.seank@gmail.com>
2019-02-24 23:33:28 -0700

    Beginning work on custom audio compression algorithm

--- Prism.Pipeline/Builtin/Audio/AudioProcessor.cs
+++ Prism.Pipeline/Builtin/Audio/AudioProcessor.cs
@@ -4,20 +4,96 @@ namespace Prism.Builtin
 {
 	// Performs the processing of audio data from the raw PCM to the custom compressed format
 	[ContentProcessor("AudioProcessor")]
-	internal class AudioProcessor : ContentProcessor<RawAudio, RawAudio, AudioWriter>
+	internal class AudioProcessor : ContentProcessor<RawAudio, RLADAudio, AudioWriter>
 	{
 		// Multiplicitive factor for converting f32 [-1,1] to s16
 		private const float F2S_FACTOR = Int16.MaxValue - 1;
 
-		public unsafe override RawAudio Process(RawAudio input, ProcessorContext ctx)
+		public unsafe override RLADAudio Process(RawAudio input, ProcessorContext ctx)
 		{
 			// Preprocessing step for mp3, convert from float to s16
 			short* data = (short*)input.Data.ToPointer();
 			if (input.Format == AudioFormat.Mp3)
 				ConvertF32ToS16(data, (float*)input.Data.ToPointer(), input.SampleCount);
 
+			//uint small = 0;
+			//uint med = 0;
+			//uint smallRun1 = 0;
+			//uint medRun1 = 0;
+			//uint smallRun2 = 0;
+			//uint medRun2 = 0;
+			//uint smallRunTotal = 0;
+			//uint medRunTotal = 0;
+			//uint smallRunCount = 0;
+			//uint medRunCount = 0;
+			//uint mismatch = 0;
+			//for (ulong si = 0; si < (input.SampleCount - 4); si += 4)
+			//{
+			//	short d1 = (short)Math.Abs(data[si + 2] - data[si]);
+			//	short d2 = (short)Math.Abs(data[si + 3] - data[si + 1]);
+			//	bool s1 = false, m1 = false, s2 = false, m2 = false;
+			//	if (d1 <= 127)
+			//	{
+			//		++small;
+			//		++smallRun1;
+			//		s1 = true;
+			//	}
+			//	else if (d1 <= 2047)
+			//	{
+			//		++med;
+			//		++medRun1;
+			//		m1 = true;
+			//	}
+			//	if (d2 <= 127)
+			//	{
+			//		++small;
+			//		++smallRun2;
+			//		s2 = true;
+			//	}
+			//	else if (d2 <= 2047)
+			//	{
+			//		++med;
+			//		++medRun2;
+			//		m2 = true;
+			//	}
+
+			//	if ((s1 || s2) && (s1 != s2))
+			//		++mismatch;
+			//	if ((m1 || m2) && (m1 != m2))
+			//		++mismatch;
+
+			//	if (d1 > 127 && smallRun1 != 0)
+			//	{
+			//		smallRunTotal += smallRun1;
+			//		++smallRunCount;
+			//	}
+			//	if (d1 > 127 && medRun1 != 0)
+			//	{
+			//		medRunTotal += medRun1;
+			//		++medRunCount;
+			//	}
+			//	if (d2 > 127 && smallRun2 != 0)
+			//	{
+			//		smallRunTotal += smallRun2;
+			//		++smallRunCount;
+			//	}
+			//	if (d2 > 127 && medRun2 != 0)
+			//	{
+			//		medRunTotal += medRun2;
+			//		++medRunCount;
+			//	}
+			//}
+
+			//ctx.Logger.Warn($"8-bit diff percentage: {(small / (float)input.SampleCount) * 100:0.000}%.");
+			//ctx.Logger.Warn($"12-bit diff percentage: {(med / (float)input.SampleCount) * 100:0.000}%.");
+
+			//ctx.Logger.Warn($"8-bit average run: {smallRunTotal / (float)smallRunCount:0.00}.");
+			//ctx.Logger.Warn($"12-bit average run: {medRunTotal / (float)smallRunCount:0.00}.");
+
+			//ctx.Logger.Warn($"Mismatch percentage: {(mismatch / input.SampleCount) * 100:0.000000}%.");
+
 			// Passthrough for now, apply compression soon
-			return input;
+			return new RLADAudio();
 		}
 
 		// For MP3, converts float PCM to s16 PCM
--- Prism.Pipeline/Builtin/Audio/AudioWriter.cs
+++ Prism.Pipeline/Builtin/Audio/AudioWriter.cs
@@ -3,23 +3,15 @@
 namespace Prism.Builtin
 {
 	// Writes audio data
-	internal class AudioWriter : ContentWriter<RawAudio>
+	internal class AudioWriter : ContentWriter<RLADAudio>
 	{
 		public override string LoaderName => "Spectrum:AudioLoader";
 
-		public override void Write(RawAudio input, ContentStream writer, WriterContext ctx)
+		public override void Write(RLADAudio input, ContentStream writer, WriterContext ctx)
 		{
 			try
 			{
-				writer.Write(input.FrameCount);
-				writer.Write(input.ChannelCount == 2); // true = stereo, false = mono
-				writer.Write(input.Rate);
 
-				unsafe
-				{
-					ulong len = (input.Format == AudioFormat.Mp3) ? input.DataLength / 2 : input.DataLength;
-					writer.Write((byte*)input.Data, (uint)len);
-				}
 			}
 			finally
 			{
