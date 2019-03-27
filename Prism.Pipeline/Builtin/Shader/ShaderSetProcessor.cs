f68714833e1d513455b98ffa603a987cc91ae28d
Sean Moss <moss.seank@gmail.com>
2019-03-27 13:53:19 -0600

    Created a mapping of glsl types to reflection integer values

--- Prism.Pipeline/Builtin/Shader/ShaderSetProcessor.cs
+++ Prism.Pipeline/Builtin/Shader/ShaderSetProcessor.cs
@@ -396,4 +396,92 @@ namespace Prism.Builtin
 		public uint Binding;
 		public uint Size; // Size in bytes
 	}
+
+	// Used to validate the types that are used in GLSL, since Spectrum only supports a subset of valid glsl types
+	// The glslangValidator tool uses the types as defined in the OpenGL headers (GL_FLOAT, GL_FLOAT_VEC2, ect...)
+	// In the source code, the mapping can be found in 'glslang/MachineIndependent/reflection.cpp' in the functions
+	//    `mapToGlType()` and `mapSamplerToGlType()`
+	// The actual values themselves are found in the `gl_types.h` file in the same directory as `reflection.cpp`
+	// The regex pattern for matching against gl_types.h is `#define (GL[A-Z0-9_]+)\W+(0x[A-F0-9]+)` and `("$1", $2),`
+	internal static class GLSLTypeValidator
+	{
+
+		public static readonly (string Name, uint Value)[] ALL_TYPES = new (string, uint)[] {
+			// Data types
+			("float", 0x1406), ("vec2", 0x8B50), ("vec3", 0x8B51), ("vec4", 0x8B52),				// FLOAT
+			("double", 0x140A), ("dvec2", 0x8FFC), ("dvec3", 0x8FFD), ("dvec4", 0x8FFE),			// DOUBLE
+			("int", 0x1404), ("ivec2", 0x8B53), ("ivec3", 0x8B54), ("ivec4", 0x8B55),				// INT32
+			("uint", 0x1405), ("uvec2", 0x8DC6), ("uvec3", 0x8DC7), ("uvec4", 0x8DC8),				// UINT32
+			("int64_t", 0x140E), ("i64vec2", 0x8FE9), ("i64vec3", 0x8FEA), ("i64vec4", 0x8FEB),		// INT64
+			("uint64_t", 0x140F), ("u64vec2", 0x8FE5), ("u64vec3", 0x8FE6), ("u64vec4", 0x8FE7),	// UINT64
+			("bool", 0x8B56), ("bvec2", 0x8B57), ("bvec3", 0x8B58), ("bvec4", 0x8B59),				// BOOL
+			("mat2", 0x8B5A), ("mat3", 0x8B5B), ("mat4", 0x8B5C),									// SQUARE FLOAT MATRIX
+			("mat2x3", 0x8B65), ("mat2x4", 0x8B66), ("mat3x2", 0x8B67), ("mat3x4", 0x8B68), ("mat4x2", 0x8B69), ("mat4x3", 0x8B6A),			// RECT FLOAT MATRIX
+			("dmat2", 0x8F46), ("dmat3", 0x8F47), ("dmat4", 0x8F48),								// SQUARE DOUBLE MATRIX
+			("dmat2x3", 0x8F49), ("dmat2x4", 0x8F4A), ("dmat3x2", 0x8F4B), ("dmat3x4", 0x8F4C), ("dmat4x2", 0x8F4D), ("dmat4x3", 0x8F4E),	// RECT DOUBLE MATRIX
+
+			// Extended float data types
+			("float16_t", 0x8FF8), ("f16vec2", 0x8FF9), ("f16vec3", 0x8FFA), ("f16vec4", 0x8FFB),	// HALF FLOAT
+			("f16mat2", 0x91C5), ("f16mat3", 0x91C6), ("f16mat4", 0x91C7),							// SQUARE HALF FLOAT MATRIX
+			("f16mat2x3", 0x91C8), ("f16mat2x4", 0x91C9), ("f16mat3x2", 0x91CA), ("f16mat3x4", 0x91CB), ("f16mat4x2", 0x91CC), ("f16mat4x3", 0x91CD), // RECT HALF FLOAT MATRIX
+
+			// Standard combined image-samplers
+			("sampler1D", 0x8B5D), ("sampler2D", 0x8B5E), ("sampler3D", 0x8B5F), ("samplerCube", 0x8B60),		// STANDARD NON-ARRAY SAMPLERS
+			("samplerBuffer", 0x8DC2),																			// SAMPLER BUFFER / UNIFORM TEXEL BUFFER
+			("sampler1DArray", 0x8DC0), ("sampler2DArray", 0x8DC1),												// STANDARD ARRAY SAMPLERS
+			("sampler1DArrayShadow", 0x8DC3), ("sampler2DArrayShadow", 0x8DC4), ("samplerCubeShadow", 0x8DC5), ("sampler1DShadow", 0x8B61), ("sampler2DShadow", 0x8B62), // STANDARD SHADOW SAMPLERS
+			("sampler2DRect", 0x8B63), ("sampler2DRectShadow", 0x8B64),											// RECT SAMPLERS
+			("sampler2DMS", 0x9108), ("sampler2DMSArray", 0x910B),												// MULTISAMPLE SAMPLERS
+			("samplerCubeArray", 0x900C), ("samplerCubeArrayShadow", 0x900D),									// CUBEMAP ARRAY SAMPLERS
+
+			// Extended float storage images and image-samplers
+			("f16sampler1D", 0x91CE), ("f16sampler2D", 0x91CF), ("f16sampler3D", 0x91D0), ("f16samplerCube", 0x91D1),	// STANDARD NON_ARRAY HALF FLOAT SAMPLERS
+			("f16Sampler2DRect", 0x91D2), ("f16sampler1DArray", 0x91D3), ("f16sampler2DArray", 0x91D4), ("f16samplerCubeArray", 0x91D5),	// HALF FLOAT RECT AND STANDARD ARRAY SAMPLERS
+			("f16SamplerBuffer", 0x91D6),																				// HALF FLOAT SAMPLER BUFFER
+			("f16sampler2DMS", 0x91D7), ("f16sampler2DMSArray", 0x91D8),												// HALF FLOAT MULTISAMPLE SAMPLERS
+			("f16sampler1DShadow", 0x91D9), ("f16sampler2DShadow", 0x91DA), ("f16sampler2DRectShadow", 0x91DB),			// STANDARD HALF FLOAT SHADOW SAMPLERS
+			("f16sampler1DArrayShadow", 0x91DC), ("f16sampler2DArrayShadow", 0x91DD),									// HALF FLOAT ARRAY SHADOW SAMPLERS
+			("f16samplerCubeShadow", 0x91DE), ("f16samplerCubeArrayShadow", 0x91DF),									// HALF FLOAT SHADOW CUBE SAMPLERS
+			("f16image1D", 0x91E0),	("f16image2D", 0x91E1),	("f16image3D", 0x91E2), ("f16image2DRect", 0x91E3),			// STANDARD HALF FLOAT STORAGE IMAGES
+			("f16imageCube", 0x91E4),																					// HALF FLOAT CUBE STORAGE IMAGE
+			("f16image1DArray", 0x91E5), ("f16image2DArray", 0x91E6), ("f16imageCubeArray", 0x91E7),					// HALF FLOAT STORAGE IMAGE ARRAYS
+			("f16imageBuffer", 0x91E8),																					// HALF FLOAT IMAGE BUFFER
+			("f16image2DMS", 0x91E9), ("f16image2DMSArray", 0x91EA),													// HALF FLOAT MULTISAMPLE IMAGES
+
+			// Standard integer image-samplers
+			("isampler1D", 0x8DC9), ("isampler2D", 0x8DCA), ("isampler3D", 0x8DCB), ("isamplerCube", 0x8DCC),			// STANDARD INT32 SAMPLERS
+			("isampler1DArray", 0x8DCE), ("isampler2DArray", 0x8DCF), ("isampler2DRect", 0x8DCD),						// INT32 ARRAY AND RECT SAMPLERS
+			("isamplerBuffer", 0x8DD0),																					// INT32 SAMPLER BUFFER
+			("isampler2DMS", 0x9109), ("isampler2DMSArray", 0x910C),													// INT32 MULTISAMPLER SAMPLERS
+			("isamplerCubeArray", 0x900E),																				// INT32 CUBE ARRAY SAMPLER
+			("usampler1D", 0x8DD1), ("usampler2D", 0x8DD2), ("usampler3D", 0x8DD3), ("usamplerCube", 0x8DD4),			// STANDARD UINT32 SAMPLERS
+			("usampler1DArray", 0x8DD6), ("usampler2DArray", 0x8DD7), ("usampler2DRect", 0x8DD5),						// UINT32 ARRAY AND RECT SAMPLERS
+			("usamplerBuffer", 0x8DD8),																					// UINT32 SAMPLER BUFFER
+			("usampler2DMSArray", 0x910D),																				// UINT32 MULTISAMPLE ARRAY SAMPLER
+			("usamplerCubeArray", 0x900F),																				// UINT32 CUBE ARRAY SAMPLER
+			("usampler2DMS", 0x910A),																					// UINT32 MULTISAMPLE SAMPLER
+
+			// Standard storage images
+			("image1D", 0x904C), ("image2D", 0x904D), ("image3D", 0x904E), ("image2DRect", 0x904F),	("imageCube", 0x9050),			// STANDARD STORAGE IMAGES
+			("imageBuffer", 0x9051),																								// STORAGE IMAGE BUFFER
+			("image1DArray", 0x9052), ("image2DArray", 0x9053),	("imageCubeArray", 0x9054),											// STORAGE IMAGE ARRAYS
+			("image2DMS", 0x9055), ("image2DMSArray", 0x9056),																		// MULTISAMPLE STORAGE IMAGES
+			("iimage1D", 0x9057), ("iimage2D", 0x9058), ("iimage3D", 0x9059), ("iimage2DRect", 0x905A),	("iimageCube", 0x905B),		// INT32 STORAGE IMAGES
+			("iimageBuffer", 0x905C),																								// INT32 STORAGE IMAGE BUFFER
+			("iimage1DArray", 0x905D), ("iimage2DArray", 0x905E), ("iimageCubeArray", 0x905F),										// INT32 STORAGE IMAGE ARRAYS
+			("iimage2DMS", 0x9060), ("iimage2DMSArray", 0x9061),																	// MULTISAMPLE INT32 STORAGE IMAGES
+			("uimage1D", 0x9062), ("uimage2D", 0x9063), ("uimage3D", 0x9064), ("uimage2DRect", 0x9065), ("uimageCube", 0x9066),		// UINT32 STORAGE IMAGES
+			("uimageBuffer", 0x9067),																								// UINT32 STORAGE IMAGE BUFFER
+			("uimage1DArray", 0x9068), ("uimage2DArray", 0x9069), ("uimageCubeArray", 0x906A),										// UINT32 STORAGE IMAGE ARRAYS
+			("uimage2DMS", 0x906B), ("uimage2DMSArray", 0x906C),																	// MULTISAMPLE UINT32 STORAGE IMAGES
+
+			// Atomic counter
+			("atomic_uint", 0x92DB),
+
+			// Subpass inputs (dont have an OpenGL equivalent, so the reflection dump assigns them a value of 0)
+			// The reflection dump also assigns blocks to have a value of 0, but they are never mixed with subpass inputs, so this will work
+			// TODO: This does not allow us to differentiate between different subpass input types, so we need to look into this
+			("subpassInput", 0x0000)
+		};
+	}
 }
