98c61b24e449ef8c673856ac40d423ab05e7d1ec
Sean Moss <moss.seank@gmail.com>
2019-06-07 22:21:21 -0600

    Initial re-implementation of Pipeline, now a full render state

---
+++ Spectrum/Graphics/Pipeline/Pipeline.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Vk = VulkanCore;
+
+namespace Spectrum.Graphics
+{
+	/// <summary>
+	/// Fully describes a rendering state, such as the vertex format, shader program, and fixed function settings.
+	/// </summary>
+	/// <remarks>Encapsulates multiple Vulkan objects into one type: a Pipeline, a RenderPass, and a Framebuffer.</remarks>
+	public class Pipeline
+	{
+		private static readonly Vk.PipelineDynamicStateCreateInfo DEFAULT_DYNAMIC_STATE = new Vk.PipelineDynamicStateCreateInfo(
+			Vk.DynamicState.Viewport, Vk.DynamicState.Scissor
+		);
+
+		#region Fields
+		/// <summary>
+		/// Optional name for the pipeline for identification and debugging purposes.
+		/// </summary>
+		public readonly string Name;
+		/// <summary>
+		/// The graphics device for the pipeline.
+		/// </summary>
+		public GraphicsDevice Device => SpectrumApp.Instance.GraphicsDevice;
+
+		// The pipeline objects
+		internal readonly Vk.Pipeline VkPipeline;
+		internal readonly Vk.PipelineLayout VkPipelineLayout;
+		internal readonly Vk.RenderPass VkRenderPass;
+		#endregion // Fields
+
+		/// <summary>
+		/// Creates a new render pipeline using the given description.
+		/// </summary>
+		/// <param name="desc">The pipeline description. <see cref="PipelineDescription.IsComplete"/> must return <c>true</c>.</param>
+		/// <param name="name">An optional name for the pipeline for debug purposes.</param>
+		public Pipeline(PipelineDescription desc, string name = null)
+		{
+			if (desc == null)
+				throw new ArgumentNullException(nameof(desc));
+			if (!desc.IsComplete)
+				throw new InvalidOperationException("Cannot create a pipeline from an incomplete description.");
+			Name = name;
+
+			// Create the layout
+			var lci = new Vk.PipelineLayoutCreateInfo(null, null);
+			VkPipelineLayout = Device.VkDevice.CreatePipelineLayout(lci);
+
+			// Non-user-specified create infos
+			var vsci = new Vk.PipelineViewportStateCreateInfo(
+				// TODO
+			);
+			var mssci = new Vk.PipelineMultisampleStateCreateInfo(Vk.SampleCounts.Count1); // TODO: derive this from the framebuffer
+
+			// Pipeline creation
+			var gpci = new Vk.GraphicsPipelineCreateInfo(
+				VkPipelineLayout,
+				null, // TODO
+				0,
+				desc._shaderCIs,
+				desc._piCI.Value,
+				desc._vdCI.Value,
+				desc._rsCI.Value,
+				tessellationState: null, // TODO: control this once we support tessellation shaders
+				viewportState: vsci,
+				multisampleState: mssci,
+				depthStencilState: desc._dssCI.Value,
+				colorBlendState: desc._cbsCI.Value,
+				dynamicState: DEFAULT_DYNAMIC_STATE,
+				flags: Vk.PipelineCreateFlags.None, // TODO: look into derivative pipelines
+				basePipelineHandle: null, // TODO: look into derivative pipelines
+				basePipelineIndex: -1 // TODO: look into derivative pipelines
+			);
+			VkPipeline = Device.VkDevice.CreateGraphicsPipeline(gpci, null, null);
+		}
+	}
+}
---
+++ Spectrum/Graphics/Pipeline/PipelineDescription.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using Vk = VulkanCore;
+
+namespace Spectrum.Graphics
+{
+	/// <summary>
+	/// Contains the set of values that fully describe a <see cref="Pipeline"/>.
+	/// </summary>
+	public class PipelineDescription
+	{
+		#region Fields
+		#region Backing Fields
+		private ColorBlendState? _colorBlendState = null;
+		private DepthStencilState? _depthStencilState = null;
+		private PrimitiveInput? _primitiveInput = null;
+		private RasterizerState? _rasterizerState = null;
+		private VertexDescription? _vertexDescription = null;
+		private Shader _shader = null;
+
+		// Vulkan pipeline create infos
+		internal Vk.PipelineColorBlendStateCreateInfo? _cbsCI = null;
+		internal Vk.PipelineDepthStencilStateCreateInfo? _dssCI = null;
+		internal Vk.PipelineInputAssemblyStateCreateInfo? _piCI = null;
+		internal Vk.PipelineRasterizationStateCreateInfo? _rsCI = null;
+		internal Vk.PipelineVertexInputStateCreateInfo? _vdCI = null;
+		internal Vk.PipelineShaderStageCreateInfo[] _shaderCIs = null;
+		#endregion // Backing Fields
+
+		#region Public Settings
+
+		public ColorBlendState? ColorBlendState
+		{
+			get { return _colorBlendState; }
+			set
+			{
+				_colorBlendState = value;
+				_cbsCI = value.HasValue ? value.Value.ToCreateInfo() : (Vk.PipelineColorBlendStateCreateInfo?)null;
+			}
+		}
+
+		public DepthStencilState? DepthStencilState
+		{
+			get { return _depthStencilState; }
+			set
+			{
+				_depthStencilState = value;
+				_dssCI = value.HasValue ? value.Value.ToCreateInfo() : (Vk.PipelineDepthStencilStateCreateInfo?)null;
+			}
+		}
+
+		public PrimitiveInput? PrimitiveInput
+		{
+			get { return _primitiveInput; }
+			set
+			{
+				_primitiveInput = value;
+				_piCI = value.HasValue ? value.Value.ToCreateInfo() : (Vk.PipelineInputAssemblyStateCreateInfo?)null;
+			}
+		}
+
+		public RasterizerState? RasterizerState
+		{
+			get { return _rasterizerState; }
+			set
+			{
+				_rasterizerState = value;
+				_rsCI = value.HasValue ? value.Value.ToCreateInfo() : (Vk.PipelineRasterizationStateCreateInfo?)null;
+			}
+		}
+
+		public VertexDescription? VertexDescription
+		{
+			get { return _vertexDescription; }
+			set
+			{
+				_vertexDescription = value;
+				_vdCI = value.HasValue ? value.Value.ToCreateInfo() : (Vk.PipelineVertexInputStateCreateInfo?)null;
+			}
+		}
+
+		public Shader Shader
+		{
+			get { return _shader; }
+			set
+			{
+				_shader = value;
+				_shaderCIs = value.CreateInfo;
+			}
+		}
+		#endregion // Public Settings
+
+		#region Checking
+		/// <summary>
+		/// Gets if the descrption has a specified color blend state.
+		/// </summary>
+		public bool HasColorBlendState => _colorBlendState.HasValue;
+		/// <summary>
+		/// Gets if the descrption has a specified depth stencil state.
+		/// </summary>
+		public bool HasDepthStencilState => _depthStencilState.HasValue;
+		/// <summary>
+		/// Gets if the descrption has a specified primitive input.
+		/// </summary>
+		public bool HasPrimitiveInput => _primitiveInput.HasValue;
+		/// <summary>
+		/// Gets if the descrption has a specified rasterizer state.
+		/// </summary>
+		public bool HasRasterizerState => _rasterizerState.HasValue;
+		/// <summary>
+		/// Gets if the descrption has a specified vertex description.
+		/// </summary>
+		public bool HasVertexDescription => _vertexDescription.HasValue;
+		/// <summary>
+		/// Gets if the descrption has a specified shader.
+		/// </summary>
+		public bool HasShader => (_shader != null);
+
+		/// <summary>
+		/// Gets if this can fully describe a pipeline. If this is <c>false</c>, then attempting to create a
+		/// <see cref="Pipeline"/> instance with this description will fail.
+		/// </summary>
+		public bool IsComplete =>
+			_colorBlendState.HasValue && _depthStencilState.HasValue && _primitiveInput.HasValue && 
+			_rasterizerState.HasValue && _vertexDescription.HasValue && (_shader != null);
+		#endregion // Checking
+		#endregion // Fields
+	}
+}
