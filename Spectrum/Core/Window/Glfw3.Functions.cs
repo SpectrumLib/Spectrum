c4b44ab82fbcc53da753b4c5d1989acd67fcdbd5
Sean Moss <moss.seank@gmail.com>
2019-10-17 20:19:38 -0400

    Finished adding in glfw functions

--- Spectrum/Core/Window/Glfw3.Functions.cs
+++ Spectrum/Core/Window/Glfw3.Functions.cs
@@ -19,6 +19,37 @@ namespace Spectrum
 		private readonly Delegates.glfwTerminate _glfwTerminate;
 		private readonly Delegates.glfwGetVersion _glfwGetVersion;
 		private readonly Delegates.glfwSetErrorCallback _glfwSetErrorCallback;
+		private readonly Delegates.glfwWindowHint _glfwWindowHint;
+		private readonly Delegates.glfwCreateWindow _glfwCreateWindow;
+		private readonly Delegates.glfwDestroyWindow _glfwDestroyWindow;
+		private readonly Delegates.glfwWindowShouldClose _glfwWindowShouldClose;
+		private readonly Delegates.glfwPollEvents _glfwPollEvents;
+		private readonly Delegates.glfwShowWindow _glfwShowWindow;
+		private readonly Delegates.glfwVulkanSupported _glfwVulkanSupported;
+		private readonly Delegates.glfwGetWindowAttrib _glfwGetWindowAttrib;
+		private readonly Delegates.glfwSetWindowAttrib _glfwSetWindowAttrib;
+		private readonly Delegates.glfwGetWindowSize _glfwGetWindowSize;
+		private readonly Delegates.glfwSetWindowSize _glfwSetWindowSize;
+		private readonly Delegates.glfwGetWindowPos _glfwGetWindowPos;
+		private readonly Delegates.glfwSetWindowPos _glfwSetWindowPos;
+		private readonly Delegates.glfwGetPrimaryMonitor _glfwGetPrimaryMonitor;
+		private readonly Delegates.glfwGetMonitors _glfwGetMonitors;
+		private readonly Delegates.glfwGetMonitorPos _glfwGetMonitorPos;
+		private readonly Delegates.glfwGetVideoModes _glfwGetVideoModes;
+		private readonly Delegates.glfwGetVideoMode _glfwGetVideoMode;
+		private readonly Delegates.glfwSetWindowTitle _glfwSetWindowTitle;
+		private readonly Delegates.glfwSetMouseButtonCallback _glfwSetMouseButtonCallback;
+		private readonly Delegates.glfwSetScrollCallback _glfwSetScrollCallback;
+		private readonly Delegates.glfwSetKeyCallback _glfwSetKeyCallback;
+		private readonly Delegates.glfwGetCursorPos _glfwGetCursorPos;
+		private readonly Delegates.glfwSetInputMode _glfwSetInputMode;
+		private readonly Delegates.glfwSetCursorEnterCallback _glfwSetCursorEnterCallback;
+		private readonly Delegates.glfwSetWindowPosCallback _glfwSetWindowPosCallback;
+		private readonly Delegates.glfwSetWindowSizeCallback _glfwSetWindowSizeCallback;
+		private readonly Delegates.glfwSetWindowFocusCallback _glfwSetWindowFocusCallback;
+		private readonly Delegates.glfwSetWindowIconifyCallback _glfwSetWindowIconifyCallback;
+		private readonly Delegates.glfwGetPhysicalDevicePresentationSupport _glfwGetPhysicalDevicePresentationSupport;
+		private readonly Delegates.glfwCreateWindowSurface _glfwCreateWindowSurface;
 		#endregion // Delegate Fields
 
 		private T loadFunc<T>()
@@ -28,6 +59,22 @@ namespace Spectrum
 		#region Public Delegate Types
 		[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
 		public delegate void GLFWerrorfun(int error, string desc);
+		[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+		public delegate void GLFWmousebuttonfun(IntPtr window, int button, int action, int mods);
+		[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+		public delegate void GLFWscrollfun(IntPtr window, double xoffset, double yoffset);
+		[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+		public delegate void GLFWkeyfun(IntPtr window, int key, int scancode, int action, int mods);
+		[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+		public delegate void Glfwcursorenterfun(IntPtr window, int entered);
+		[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+		public delegate void GLFWwindowposfun(IntPtr window, int xpos, int ypos);
+		[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+		public delegate void GLFWwindowsizefun(IntPtr window, int width, int height);
+		[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+		public delegate void GLFWwindowfocusfun(IntPtr window, int focused);
+		[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+		public delegate void GLFWwindowiconifyfun(IntPtr window, int iconified);
 		#endregion // Public Delegate Types
 
 		#region API Delegate Types
@@ -41,6 +88,68 @@ namespace Spectrum
 			public delegate void glfwGetVersion(out int maj, out int min, out int rev);
 			[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
 			public delegate void glfwSetErrorCallback(GLFWerrorfun cbfun);
+			[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+			public delegate void glfwWindowHint(int hint, int value);
+			[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+			public delegate IntPtr glfwCreateWindow(int width, int height, IntPtr title, IntPtr monitor, IntPtr share);
+			[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+			public delegate void glfwDestroyWindow(IntPtr window);
+			[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+			public delegate int glfwWindowShouldClose(IntPtr window);
+			[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+			public delegate void glfwPollEvents();
+			[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+			public delegate void glfwShowWindow(IntPtr window);
+			[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+			public delegate int glfwVulkanSupported();
+			[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+			public delegate int glfwGetWindowAttrib(IntPtr window, int attrib);
+			[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+			public delegate void glfwSetWindowAttrib(IntPtr window, int attrib, int value);
+			[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+			public delegate void glfwGetWindowSize(IntPtr window, out int width, out int height);
+			[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+			public delegate void glfwSetWindowSize(IntPtr window, int width, int height);
+			[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+			public delegate void glfwGetWindowPos(IntPtr window, out int x, out int y);
+			[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+			public delegate void glfwSetWindowPos(IntPtr window, int w, int y);
+			[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+			public delegate IntPtr glfwGetPrimaryMonitor();
+			[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+			public delegate IntPtr glfwGetMonitors(out int count);
+			[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+			public delegate void glfwGetMonitorPos(IntPtr monitor, out int x, out int y);
+			[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+			public delegate IntPtr glfwGetVideoModes(IntPtr monitor, out int count);
+			[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+			public delegate IntPtr glfwGetVideoMode(IntPtr monitor);
+			[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+			public delegate void glfwSetWindowTitle(IntPtr window, IntPtr title);
+			[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+			public delegate GLFWmousebuttonfun glfwSetMouseButtonCallback(IntPtr window, GLFWmousebuttonfun mouse_button_callback);
+			[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+			public delegate GLFWscrollfun glfwSetScrollCallback(IntPtr window, GLFWscrollfun scroll_callback);
+			[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+			public delegate GLFWkeyfun glfwSetKeyCallback(IntPtr window, GLFWkeyfun key_callback);
+			[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+			public delegate void glfwGetCursorPos(IntPtr window, out double xpos, out double ypos);
+			[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+			public delegate void glfwSetInputMode(IntPtr window, int mode, int value);
+			[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+			public delegate void glfwSetCursorEnterCallback(IntPtr window, Glfwcursorenterfun cursor_enter_callback);
+			[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+			public delegate void glfwSetWindowPosCallback(IntPtr window, GLFWwindowposfun window_pos_callback);
+			[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+			public delegate void glfwSetWindowSizeCallback(IntPtr window, GLFWwindowsizefun window_size_callback);
+			[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+			public delegate void glfwSetWindowFocusCallback(IntPtr window, GLFWwindowfocusfun focus_callback);
+			[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+			public delegate void glfwSetWindowIconifyCallback(IntPtr window, GLFWwindowiconifyfun iconify_callback);
+			[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+			public delegate int glfwGetPhysicalDevicePresentationSupport(IntPtr instance, IntPtr device, uint family);
+			[UnmanagedFunctionPointer(CallingConvention.Cdecl), SuppressUnmanagedCodeSecurity]
+			public delegate int glfwCreateWindowSurface(IntPtr instance, IntPtr window, IntPtr alloc, out IntPtr surface);
 		}
 		#endregion // API Delegate Types
 	}
--- Spectrum/Core/Window/Glfw3.cs
+++ Spectrum/Core/Window/Glfw3.cs
@@ -4,12 +4,17 @@
  * the 'LICENSE' file at the root of this repository, or online at <https://opensource.org/licenses/MS-PL>.
  */
 using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
 
 namespace Spectrum
 {
 	internal partial class Glfw3 : IDisposable
 	{
 		#region Fields
+		private IntPtr _handle = IntPtr.Zero;
+
 		private int _lastErrorCode = Glfw3.NO_ERROR;
 		private string _lastErrorDesc = String.Empty;
 		// A tuple of the last error code and description, which clears the error values
@@ -26,12 +31,15 @@ namespace Spectrum
 		// Gets if there is an error
 		public bool HasError => _lastErrorCode != Glfw3.NO_ERROR;
 
-		private IntPtr _handle = IntPtr.Zero;
+		// Gets time it took to load the library and functions
+		public readonly TimeSpan LoadTime;
 		#endregion // Fields
 
 		// Loads the library and function pointers
 		public Glfw3()
 		{
+			Stopwatch timer = Stopwatch.StartNew();
+
 			_handle = Native.NativeLoader.LoadLibrary("glfw3", "libglfw.so.3");
 
 			// Check the version
@@ -44,22 +52,140 @@ namespace Spectrum
 			_glfwInit = loadFunc<Delegates.glfwInit>();
 			_glfwTerminate = loadFunc<Delegates.glfwTerminate>();
 			_glfwSetErrorCallback = loadFunc<Delegates.glfwSetErrorCallback>();
+			_glfwWindowHint = loadFunc<Delegates.glfwWindowHint>();
+			_glfwCreateWindow = loadFunc<Delegates.glfwCreateWindow>();
+			_glfwDestroyWindow = loadFunc<Delegates.glfwDestroyWindow>();
+			_glfwWindowShouldClose = loadFunc<Delegates.glfwWindowShouldClose>();
+			_glfwPollEvents = loadFunc<Delegates.glfwPollEvents>();
+			_glfwShowWindow = loadFunc<Delegates.glfwShowWindow>();
+			_glfwVulkanSupported = loadFunc<Delegates.glfwVulkanSupported>();
+			_glfwGetWindowAttrib = loadFunc<Delegates.glfwGetWindowAttrib>();
+			_glfwSetWindowAttrib = loadFunc<Delegates.glfwSetWindowAttrib>();
+			_glfwGetWindowSize = loadFunc<Delegates.glfwGetWindowSize>();
+			_glfwSetWindowSize = loadFunc<Delegates.glfwSetWindowSize>();
+			_glfwGetWindowPos = loadFunc<Delegates.glfwGetWindowPos>();
+			_glfwSetWindowPos = loadFunc<Delegates.glfwSetWindowPos>();
+			_glfwGetPrimaryMonitor = loadFunc<Delegates.glfwGetPrimaryMonitor>();
+			_glfwGetMonitors = loadFunc<Delegates.glfwGetMonitors>();
+			_glfwGetMonitorPos = loadFunc<Delegates.glfwGetMonitorPos>();
+			_glfwGetVideoModes = loadFunc<Delegates.glfwGetVideoModes>();
+			_glfwGetVideoMode = loadFunc<Delegates.glfwGetVideoMode>();
+			_glfwSetWindowTitle = loadFunc<Delegates.glfwSetWindowTitle>();
+			_glfwSetMouseButtonCallback = loadFunc<Delegates.glfwSetMouseButtonCallback>();
+			_glfwSetScrollCallback = loadFunc<Delegates.glfwSetScrollCallback>();
+			_glfwSetKeyCallback = loadFunc<Delegates.glfwSetKeyCallback>();
+			_glfwGetCursorPos = loadFunc<Delegates.glfwGetCursorPos>();
+			_glfwSetInputMode = loadFunc<Delegates.glfwSetInputMode>();
+			_glfwSetCursorEnterCallback = loadFunc<Delegates.glfwSetCursorEnterCallback>();
+			_glfwSetWindowPosCallback = loadFunc<Delegates.glfwSetWindowPosCallback>();
+			_glfwSetWindowSizeCallback = loadFunc<Delegates.glfwSetWindowSizeCallback>();
+			_glfwSetWindowFocusCallback = loadFunc<Delegates.glfwSetWindowFocusCallback>();
+			_glfwSetWindowIconifyCallback = loadFunc<Delegates.glfwSetWindowIconifyCallback>();
+			_glfwGetPhysicalDevicePresentationSupport = loadFunc<Delegates.glfwGetPhysicalDevicePresentationSupport>();
+			_glfwCreateWindowSurface = loadFunc<Delegates.glfwCreateWindowSurface>();
 
 			// Set the error callback
 			_glfwSetErrorCallback((code, desc) => {
 				_lastErrorCode = code;
 				_lastErrorDesc = desc;
 			});
+
+			LoadTime = timer.Elapsed;
 		}
 		~Glfw3()
 		{
 			Dispose();
 		}
 
-		#region API Functions
+		#region Passthrough API Functions
 		public bool Init() => (_glfwInit() == Glfw3.TRUE);
 		public void Terminate() => _glfwTerminate();
-		#endregion // API Functions
+		public void WindowHint(int hint, int value) => _glfwWindowHint(hint, value);
+		public void DestroyWindow(IntPtr window) => _glfwDestroyWindow(window);
+		public bool WindowShouldClose(IntPtr window) => (_glfwWindowShouldClose(window) == Glfw3.TRUE);
+		public void PollEvents() => _glfwPollEvents();
+		public void ShowWindow(IntPtr window) => _glfwShowWindow(window);
+		public bool VulkanSupported() => (_glfwVulkanSupported() == Glfw3.TRUE);
+		public int GetWindowAttrib(IntPtr window, int attrib) => _glfwGetWindowAttrib(window, attrib);
+		public void SetWindowAttrib(IntPtr window, int attrib, int value) => _glfwSetWindowAttrib(window, attrib, value);
+		public void GetWindowSize(IntPtr window, out int w, out int h) => _glfwGetWindowSize(window, out w, out h);
+		public void SetWindowSize(IntPtr window, int w, int h) => _glfwSetWindowSize(window, w, h);
+		public void GetWindowPos(IntPtr window, out int x, out int y) => _glfwGetWindowPos(window, out x, out y);
+		public void SetWindowPos(IntPtr window, int x, int y) => _glfwSetWindowPos(window, x, y);
+		public IntPtr GetPrimaryMonitor() => _glfwGetPrimaryMonitor();
+		public void GetMonitorPos(IntPtr monitor, out int x, out int y) => _glfwGetMonitorPos(monitor, out x, out y);
+		public void SetMouseButtonCallback(IntPtr window, GLFWmousebuttonfun mouse_button_callback)
+			=> _glfwSetMouseButtonCallback(window, mouse_button_callback);
+		public void SetScrollCallback(IntPtr window, GLFWscrollfun scroll_callback)
+			=> _glfwSetScrollCallback(window, scroll_callback);
+		public void SetKeyCallback(IntPtr window, GLFWkeyfun key_callback) => _glfwSetKeyCallback(window, key_callback);
+		public void GetCursorPos(IntPtr window, out double x, out double y) => _glfwGetCursorPos(window, out x, out y);
+		public void SetInputMode(IntPtr window, int mode, int value) => _glfwSetInputMode(window, mode, value);
+		public void SetCursorEnterCallback(IntPtr window, Glfwcursorenterfun func) => _glfwSetCursorEnterCallback(window, func);
+		public void SetWindowPosCallback(IntPtr window, GLFWwindowposfun func) => _glfwSetWindowPosCallback(window, func);
+		public void SetWindowSizeCallback(IntPtr window, GLFWwindowsizefun func) => _glfwSetWindowSizeCallback(window, func);
+		public void SetWindowFocusCallback(IntPtr window, GLFWwindowfocusfun func) => _glfwSetWindowFocusCallback(window, func);
+		public void SetWindowIconifyCallback(IntPtr window, GLFWwindowiconifyfun func) => _glfwSetWindowIconifyCallback(window, func);
+		// TODO: Re-add when vulkan is linked in
+		//public bool GetPhysicalDevicePresentationSupport(Vk.Instance inst, Vk.PhysicalDevice dev, uint fam)
+		//	=> (_glfwGetPhysicalDevicePresentationSupport(inst.Handle, dev.Handle, fam) == 1);
+		#endregion // Passthrough API Functions
+
+		#region API Function Wrappers
+		public unsafe IntPtr CreateWindow(int width, int height, string title)
+		{
+			byte[] tstr = Encoding.UTF8.GetBytes(title + '\0');
+
+			fixed (byte* tptr = tstr)
+			{
+				return _glfwCreateWindow(width, height, (IntPtr)tptr, IntPtr.Zero, IntPtr.Zero);
+			}
+		}
+
+		public IntPtr[] GetMonitors()
+		{
+			IntPtr mptr = _glfwGetMonitors(out int count);
+
+			IntPtr[] mons = new IntPtr[count];
+			for (int i = 0; i < count; ++i, mptr += IntPtr.Size)
+				mons[i] = Marshal.ReadIntPtr(mptr);
+			return mons;
+		}
+
+		public VidMode[] GetVideoModes(IntPtr monitor)
+		{
+			IntPtr mptr = _glfwGetVideoModes(monitor, out int count);
+
+			VidMode[] modes = new VidMode[count];
+			for (int i = 0; i < count; ++i, mptr += (6 * sizeof(int)))
+				modes[i] = Marshal.PtrToStructure<VidMode>(mptr);
+			return modes;
+		}
+
+		public VidMode GetVideoMode(IntPtr monitor)
+		{
+			IntPtr mptr = _glfwGetVideoMode(monitor);
+			return Marshal.PtrToStructure<VidMode>(mptr);
+		}
+
+		public unsafe void SetWindowTitle(IntPtr window, string title)
+		{
+			byte[] tstr = Encoding.UTF8.GetBytes(title + '\0');
+			fixed (byte* tptr = tstr)
+			{
+				_glfwSetWindowTitle(window, (IntPtr)tptr);
+			}
+		}
+
+		// TODO: Re-add when vulkan is linked in
+		//public static long CreateWindowSurface(Vk.Instance inst, IntPtr window)
+		//{
+		//	Vk.Result res = (Vk.Result)_glfwCreateWindowSurface(inst.Handle, window, IntPtr.Zero, out IntPtr surface);
+		//	if (res != Vk.Result.Success)
+		//		throw new Vk.VulkanException(res, "Could not create Vulkan surface from GLFW.");
+		//	return surface.ToInt64();
+		//}
+		#endregion // API Function Wrappers
 
 		#region IDisposable
 		public void Dispose()
@@ -71,5 +197,24 @@ namespace Spectrum
 			}
 		}
 		#endregion // IDisposable
+
+		#region Structs
+		[StructLayout(LayoutKind.Explicit, Size=6*sizeof(int))]
+		public struct VidMode
+		{
+			[FieldOffset(0)]
+			public int Width;
+			[FieldOffset(1	*sizeof(int))]
+			public int Height;
+			[FieldOffset(2*sizeof(int))]
+			public int RedBits;
+			[FieldOffset(3*sizeof(int))]
+			public int GreenBits;
+			[FieldOffset(4*sizeof(int))]
+			public int BlueBits;
+			[FieldOffset(5*sizeof(int))]
+			public int RefreshRate;
+		}
+		#endregion // Structs
 	}
 }
