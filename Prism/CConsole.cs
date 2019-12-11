b83ffb13037ebf679c7c00d12fd32f7aa83e8d0f
Sean Moss <moss.seank@gmail.com>
2019-12-10 22:21:41 -0500

    Usage statement and Prism dependencies and ILRepack script

---
+++ Prism/CConsole.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Microsoft Public License (Ms-PL) - Copyright (c) 2018-2019 The Spectrum Team
+ * This file is subject to the terms and conditions of the Microsoft Public License, the text of which can be found in
+ * the 'LICENSE' file at the root of this repository, or online at <https://opensource.org/licenses/MS-PL>.
+ */
+using System;
+
+namespace Prism
+{
+    // Console wrapper that adds colors and message severity tags
+	internal static class CConsole
+	{
+        private static readonly ConsoleColor FORE_DEFAULT;
+
+        static CConsole()
+        {
+            FORE_DEFAULT = Console.ForegroundColor;
+        }
+
+        public static void Info(string msg) => Console.WriteLine(msg);
+
+        public static void Warn(string msg)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine($"W: {msg}");
+            Console.ForegroundColor = FORE_DEFAULT;
+        }
+
+        public static void Error(string msg)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"E: {msg}");
+            Console.ForegroundColor = FORE_DEFAULT;
+        }
+
+        public static void Verbose(string msg)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine(msg);
+            Console.ForegroundColor = FORE_DEFAULT;
+        }
+	}
+}
--- Prism/Program.cs
+++ Prism/Program.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿/*
+ * Microsoft Public License (Ms-PL) - Copyright (c) 2018-2019 The Spectrum Team
+ * This file is subject to the terms and conditions of the Microsoft Public License, the text of which can be found in
+ * the 'LICENSE' file at the root of this repository, or online at <https://opensource.org/licenses/MS-PL>.
+ */
+using System;
 
 namespace Prism
 {
-	class Program
+	internal static class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			if (args.Length == 0)
+			{
+				CConsole.Info("Usage (cmd): Prism.exe <action> <project> [args]");
+				CConsole.Info("Usage (gui): Prism.exe <project>");
+			}
 		}
 	}
 }
