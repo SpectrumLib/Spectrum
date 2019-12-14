68fe44c6db1eefc9d45199b74aeda7fe8022a3a8
Sean Moss <moss.seank@gmail.com>
2019-12-13 21:31:17 -0500

    Prism argument parsing and help text

---
+++ Prism/Arguments.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Microsoft Public License (Ms-PL) - Copyright (c) 2018-2019 The Spectrum Team
+ * This file is subject to the terms and conditions of the Microsoft Public License, the text of which can be found in
+ * the 'LICENSE' file at the root of this repository, or online at <https://opensource.org/licenses/MS-PL>.
+ */
+using System;
+using System.Linq;
+
+namespace Prism
+{
+	// Parses the program arguments
+	internal static class Arguments
+	{
+		#region Fields
+		public static string ParseError = null;
+
+		public static bool Help = false;
+		#endregion // Fields
+
+		public static bool Parse(string[] args)
+		{
+			// Prepare the arguments
+			var argpairs = new (string name, string value)[args.Length];
+			Prepare(args, argpairs);
+
+			// Check first for the help flag, exit early if present
+			if (argpairs.Any(arg => (arg.value is null) && (arg.name == "help" || arg.name == "h" || arg.name == "?")))
+			{
+				Help = true;
+				return true;
+			}
+
+			return true;
+		}
+
+		private static void Prepare(string[] args, (string name, string value)[] sanargs)
+		{
+			int aidx = 0;
+			foreach (var arg in args)
+			{
+				bool ispar = (arg.Length > 1) && ((arg[0] == '-') || (arg[0] == '/'));
+				sanargs[aidx++] = ispar ? _split_param(arg) : (null, arg);
+			}
+
+			static (string, string) _split_param(ReadOnlySpan<char> par)
+			{
+				var idx = par.IndexOfAny(':', '=');
+				var plen = (par[1] == '-') ? 2 : 1;
+				var name = (idx != -1) ? par.Slice(plen, idx - plen) : par.Slice(plen);
+				var value = (idx != -1) ? par.Slice(idx + 1) : ReadOnlySpan<char>.Empty;
+				return (name.ToString(), value.IsEmpty ? null : value.ToString());
+			}
+		}
+	}
+}
--- Prism/Program.cs
+++ Prism/Program.cs
@@ -13,9 +13,53 @@ namespace Prism
 		{
 			if (args.Length == 0)
 			{
-				CConsole.Info("Usage (cmd): Prism.exe <action> <project> [args]");
-				CConsole.Info("Usage (gui): Prism.exe <project>");
+				CConsole.Info("Usage (cmd):  Prism.exe <action> <project> [args]");
+				CConsole.Info("Usage (gui):  Prism.exe <project>");
+				CConsole.Info("Usage (help): Prism.exe /(help|h|?)");
+				return;
 			}
+
+			// Parse the arguments
+			if (!Arguments.Parse(args))
+			{
+				CConsole.Error(Arguments.ParseError);
+				return;
+			}
+
+			// Display help and exit
+			if (Arguments.Help)
+			{
+				PrintHelp();
+				return;
+			}
+		}
+
+		private static void PrintHelp()
+		{
+			Console.Write(
+				// ===========================================================================
+				"\nPrism" +
+				"\n-----" +
+				"\nPrism is an extensible build tool and project manager for content file" +
+				"\npre-processing for the Spectrum library." +
+				"\n" +
+				"\nUsage (gui):    Prism.exe <project_file>" +
+				"\nUsage (cmd):    Prism.exe <action> <project_file> [args...]" +
+				"\n   <action> can be one of:" +
+				"\n      build         - Builds the project file." +
+				"\n      rebuild       - Rebuilds the project file, ignoring the build cache." +
+				"\n      clean         - Cleans the build cache for the project file." +
+				"\n      view          - Shows a summary of the project file." +
+				"\n   The action must come as the first argument of the program for command-" +
+				"\n   line mode. The project file must be the second argument. A project file" +
+				"\n   as the first argument will cause the GUI to open." +
+				"\n" + 
+				"\nThe command line parameters are:" +
+				"\n   > help;h;?       - Prints this message, and exits immediately." +
+				"\n" + 
+				"\nFor compatiblity, all parameters can be specified with '/', '-', or '--'." +
+				"\n\n"
+			);
 		}
 	}
 }
